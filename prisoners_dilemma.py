8e4dbebc0b7a5d3482608e34d9f859ad747f049a
Christopher Duhaime <christopherduhaime17@PHS221_4346.FG.local>
2015-11-25 09:42:29 -0500

    long life beta plan

--- prisoners_dilemma.py
+++ prisoners_dilemma.py
@@ -340,21 +340,16 @@ def get_action(player, history, opponent_history, score, opponent_score, getting
 
 
 
-    ######
+    ###### MEEMEMEMEMEMEMEMEMEMEMEME
     ######
     #
     elif player == 9:
         if getting_team_name:
-            return 'loyal vengeful'
+            return 'If you betrayed me then I will visit you when I get out'
         else:
-            # use history, opponent_history, score, opponent_score
-            # to compute your strategy
-            if len(opponent_history)==0: #It's the first round: collude
+                '''This may put me in prison for long time but I will live'''
                 return 'c'
-            elif history[-1]=='c' and opponent_history[-1]=='b':
-                return 'b' # betray is they were severely punished last time
-            else:
-                return 'c' #otherwise collude
+
 
 
 
